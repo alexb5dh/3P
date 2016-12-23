827cd4e485a75549e08e0fcf8287249631e9eb9a
alexb5dh <alexb5dh@gmail.com>
2016-12-23 22:03:15 +0200

    tests modernization

--- _tests.py
+++ _tests.py
@@ -1,4 +1,4 @@
-from power import *
+from power import power
 assert power(a = 0, n = 0) == 1
 assert power(a = 0, n = 1) == 0
 assert power(a = 0, n = 10) == 0
@@ -11,3 +11,4 @@ assert power(a = -2, n = 3) == -8
 assert power(a = 2, n = 2 + 1) == 8
 assert power(a = 2, n = 7) == 128
 assert power(a = 2, n = 10) == 1024
+print("✓: " + str(power))
